b9763f2bd92dba989a4e340170f4819b4b35a350
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-07-23 20:30:57 -0700

    Hook entire process for generating HTML

--- src/mailer.ml
+++ src/mailer.ml
@@ -3,7 +3,16 @@ open Reactjs
 open Lwt.Infix
 open Nodejs_high_level_lwt
 
-let email json = DOM.(
+module S = Yojson.Safe
+module U = Yojson.Safe.Util
+
+type mailing_list = { date_range : string;
+                      articles : post list; }
+and post = {author : string;
+            title : string;
+            content : string; }
+
+let newsletter json = DOM.(
     make_class_spec
       (fun ~this -> let open Infix in
 
@@ -12,12 +21,6 @@ let email json = DOM.(
       )|> create_class
   )
 
-type mailing_list = { date_range : string;
-                      articles : post list; } [@@deriving of_yojson]
-and post = {author : string;
-            title : string;
-            content : string; }
-
 let () =
   Lwt.async (fun () ->
       let p = Nodejs_high_level.process in
@@ -25,25 +28,35 @@ let () =
 
       if List.length args <> 3
       then begin
-        "Must provide input file of posts for HTML generation"
+        "Must provide input json file of posts for HTML generation"
         |> prerr_endline;
         p#exit 1
         end;
 
       let posts_file = List.nth args 2 in
-      print_endline posts_file;
 
-      Fs.read_file posts_file >|= fun (error, data) ->
-      let j = Yojson.Safe.from_string data#to_string in
+      let read_string ~key o = U.member key o |> U.to_string in
+      Fs.read_file posts_file >|= fun (_, data) ->
+      let j = S.from_string data#to_string in
 
-      Yojson.Safe.to_string j |> print_endline;
+      let mailing_list = {
+        date_range = U.member "date_range" j |> U.to_string;
+        articles = U.member "articles" j
+                   |> U.to_list
+                   |> List.map ~f:(fun entry ->
+                       {author = read_string ~key:"author" entry;
+                        title = read_string ~key:"title" entry;
+                        content = read_string ~key:"content" entry;}
+                     )
+      }
+      in
 
-      match mailing_list_of_yojson j with
-      | Result.Ok right ->
-        print_endline "everything okay"
-      | Result.Error left ->
-        print_endline @@ "ERROR: " ^ left ;
-        p#exit 1
-
+      let rendered =
+        (Reactjs.react_dom_server ())
+        ##renderToStaticMarkup (newsletter mailing_list
+                                |> create_element_from_class)
+        |> Js.to_string
+      in
+      print_endline rendered
 
     )
